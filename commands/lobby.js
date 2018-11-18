5e2b20ba8e13538333f2e1ce16035002f4daf225
adhesivestiky <44936240+adhesivestiky@users.noreply.github.com>
2018-11-18 15:01:06 -0800

    ;D

--- commands/lobby.js
+++ commands/lobby.js
@@ -2,6 +2,7 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
+  if(!message.author.hasPermission("MANAGE_MESSAGES")) message.channel.reply("You can't do that");
   const sayMessage = args.join(" ");
   let chat = message.guild.channels.find(`name`, "main-lobby");
   
