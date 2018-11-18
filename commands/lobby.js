44be9555ce5d7d48882cc03a78289562961e8e88
adhesivestiky <44936240+adhesivestiky@users.noreply.github.com>
2018-11-18 09:16:34 -0800

    Fixed error

--- commands/lobby.js
+++ commands/lobby.js
@@ -2,10 +2,10 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-  const message = args.join(" ");
+  const sayMessage = args.join(" ");
   let chat = message.guild.channels.find(`name`, "main-chat");
   
-  chat.send(message);
+  chat.send(sayMessage);
 
 }
 
