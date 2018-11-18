bc7609e097c36087e53d1bc6566efe97738f49d2
adhesivestiky <44936240+adhesivestiky@users.noreply.github.com>
2018-11-18 14:58:19 -0800

    Corrected channel name

--- commands/lobby.js
+++ commands/lobby.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
   const sayMessage = args.join(" ");
-  let chat = message.guild.channels.find(`name`, "main-chat");
+  let chat = message.guild.channels.find(`name`, "main-lobby");
   
   chat.send(sayMessage);
 
