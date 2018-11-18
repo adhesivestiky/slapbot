4c7c08dfafcc83f429b16fa85f93391bc9e73239
adhesivestiky <44936240+adhesivestiky@users.noreply.github.com>
2018-11-18 15:39:14 -0800

    .

--- commands/hp.js
+++ commands/hp.js
@@ -6,7 +6,6 @@ module.exports.run = async (bot, message, args) => {
   let health = JSON.parse(fs.readFileSync("./health.json", "utf8"));
   let hpUser = message.mentions.members.first() || message.guild.members.get(args[0]);
   if(!hpUser) return message.reply("Specify a member of this server. Yeah, you have to mention yourself for this.");
-  let reason = args.join(" ").slice(22);
 
   if(!health[hpUser.id]) health[hpUser.id] = {
     health: 100
@@ -17,7 +16,7 @@ module.exports.run = async (bot, message, args) => {
     if (err) console.log(err);
   });
   
-    message.channel.send(`You have ${health}/100 health left.`);
+    message.channel.send(`You have ${health[hpUser.id]}/100 health left.`);
 
 
     }
