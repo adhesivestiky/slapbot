8af967e17c2217cdf50f7981d697e6ff5b8513ba
adhesivestiky <44936240+adhesivestiky@users.noreply.github.com>
2018-11-18 15:08:12 -0800

    Now only people with manage messages can use this!

--- commands/lobby.js
+++ commands/lobby.js
@@ -2,7 +2,9 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-  if(!message.member.hasPermission("MANAGE_MESSAGES")) message.channel.reply("You can't do that");
+  if(!message.member.hasPermission("MANAGE_MESSAGES"));
+  return message.reply("Sorry, you don't have permission to use this!");
+  
   const sayMessage = args.join(" ");
   let chat = message.guild.channels.find(`name`, "main-lobby");
   
