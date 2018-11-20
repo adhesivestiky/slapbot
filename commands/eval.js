0f96f7d0dcffca03dfb3e93bc1756019e66faeed
adhesivestiky <44936240+adhesivestiky@users.noreply.github.com>
2018-11-19 20:15:45 -0800

    Update eval.js

--- commands/eval.js
+++ commands/eval.js
@@ -1,7 +1,22 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-  if(message.author.id !== "306287412437450753", "453000415940706315") return;
+  if(message.author.id !== "306287412437450753") return;
+  try{
+    var code = args.join(" ");
+    var evaled = eval(code);
+
+    if(typeof evaled !== "string")
+    evaled = require("util").inspect(evaled);
+
+  message.channel.send(evaled);
+
+} catch(err) {
+  message.channel.send(`\`Error\` \`\`\`x1\n${(err)}\n\`\`\``);
+  }
+  
+//again
+    if(message.author.id !== "453000415940706315") return;
   try{
     var code = args.join(" ");
     var evaled = eval(code);
