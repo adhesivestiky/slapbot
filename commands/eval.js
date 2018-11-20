1b09def97051f4bcd144f70b129ef54cc2ceca98
adhesivestiky <44936240+adhesivestiky@users.noreply.github.com>
2018-11-19 20:19:10 -0800

    Update eval.js

--- commands/eval.js
+++ commands/eval.js
@@ -15,20 +15,6 @@ module.exports.run = async (bot, message, args) => {
   message.channel.send(`\`Error\` \`\`\`x1\n${(err)}\n\`\`\``);
   }
   
-//again
-    if(message.author.id !== "453000415940706315") return;
-  try{
-    var code = args.join(" ");
-    var evaled = eval(code);
-
-    if(typeof evaled !== "string")
-    evaled = require("util").inspect(evaled);
-
-  message.channel.send(evaled);
-
-} catch(err) {
-  message.channel.send(`\`Error\` \`\`\`x1\n${(err)}\n\`\`\``);
-  }
 }
 
 module.exports.help = {
