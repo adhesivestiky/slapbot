364420eea7f8da678699d51d33c8de8872f9316a
adhesivestiky <44936240+adhesivestiky@users.noreply.github.com>
2018-11-19 18:49:26 -0800

    added command

--- commands/balance.js
+++ commands/balance.js
@@ -1,6 +1,25 @@
 const Discord = require("discord.js");
+let money = require("./money.json");
 
 module.exports.run = async (bot, message, args) => {
+  
+  let member = message.mentions.members.first() || message.guild.members.get(args[0]);
+  if(!member){
+    member = message.author
+  };
+  
+ if(!money[member.id]){
+    money[member.id] = {
+      money: 0
+    };
+  }
+  
+  let uCash = money[member.id].money;
+  
+  let cEmbed = new Discord.RichEmbed()
+  .setAuthor(`${message.author.username}'s Balance:`)
+  .setColor("#00ff00")
+  .addField(`${message.author.tag} haaaaaaas...`, `${uCash} money!`)
   
 }
 
