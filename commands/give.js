0da802deadaf2796f54db6fd275f3e6548562cbf
adhesivestiky <44936240+adhesivestiky@users.noreply.github.com>
2018-11-19 22:38:14 -0800

    Fixed bug
    Changed variable 'coins' to 'money'

--- commands/give.js
+++ commands/give.js
@@ -1,29 +1,29 @@
 const Discord = require("discord.js");
 const fs = require("fs");
-let coins = require("../money.json");
+let money = require("../money.json");
 
 module.exports.run = async (bot, message, args) => {
- if(!coins[message.author.id]){
+ if(!money[message.author.id]){
   return message.reply("You don't even ***have*** any money... **NOTE**: Money resets when the bot gets updated and at this end, it gets updated a lot. Some users are important so they get their money saved however :3");
  }
  
  let pUser = message.mentions.members.first() || message.guild.members.get(args[0]);
- let pCoins = coins[pUser.id].coins;
- let sCoins = coins[message.author.id].coins
+ let pCoins = money[pUser.id].money;
+ let sCoins = money[message.author.id].money;
  
  if(sCoins < args[0]) return message.repy("You don't have enough money... feelsbadman");
  
- coins[message.author.id] = {
-  coins: sCoins - parseInt(args[1])
+ money[message.author.id] = {
+  money: sCoins - parseInt(args[1])
  };
  
- coins[pUser.id] = {
-  coins: pCoins + parseInt(args[1])
+ money[pUser.id] = {
+  money: pCoins + parseInt(args[1])
  };
  
  message.channel.send("Money transfer complete!");
  
- fs.writeFile("./money.json", JSON.stringify(coins), (err) => {
+ fs.writeFile("./money.json", JSON.stringify(money), (err) => {
   if(err) console.log(err)
  });
  
