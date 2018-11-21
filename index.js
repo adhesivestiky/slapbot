d1df1e3f6bf94dd4350c47c315f2d8057f90be51
adhesivestiky <44936240+adhesivestiky@users.noreply.github.com>
2018-11-20 22:32:55 -0800

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ const Discord = require("discord.js");
 const bot = new Discord.Client({disableEveryone: true});
 const fs = require("fs");
 let money = require("./money.json");
-let prefix = botconfig.prefix;
-
 
 bot.commands = new Discord.Collection();
 
@@ -37,7 +35,7 @@ bot.on("message", async message => {
   console.log(`received message ${message.content}`);
   if(message.author.bot) return;
   if(message.channel.type === "dm") return;
-  if(message.content.slice(prefix.length) !== prefix) return;
+  if (message.content.slice(1) !== botconfig.prefix) return;
   
   if(!money[message.author.id]){
     money[message.author.id] = {
