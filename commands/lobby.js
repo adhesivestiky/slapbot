8a184a6fa47e1d385c03766c94bbcc39cd279a03
adhesivestiky <44936240+adhesivestiky@users.noreply.github.com>
2018-11-20 23:03:14 -0800

    Fixed permission error

--- commands/lobby.js
+++ commands/lobby.js
@@ -2,8 +2,9 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-  if(!message.member.hasPermission("MANAGE_MESSAGES"));
+  if(!message.member.hasPermission("MANAGE_MESSAGES")){
   return message.reply("Sorry, you don't have permission to use this!");
+  }
   
   const sayMessage = args.join(" ");
   let chat = message.guild.channels.find(`name`, "main-lobby");
