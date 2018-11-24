e3730ab18af66660e01eba4f32eb795c5338d4c6
adhesivestiky <44936240+adhesivestiky@users.noreply.github.com>
2018-11-23 20:36:02 -0800

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,6 @@ bot.on("message", async message => {
   console.log(`received message ${message.content} from ${message.author.tag}`);
   if(message.author.bot) return;
   if(message.channel.type === "dm") return;
-  if(message.content.slice(prefix.length) !== prefix) return;
   
   if(!money[message.author.id]){
     money[message.author.id] = {
