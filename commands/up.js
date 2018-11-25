81e5cf7e4d99069e736a851087514b227ed0cd06
adhesivestiky <44936240+adhesivestiky@users.noreply.github.com>
2018-11-24 22:50:14 -0800

    Update up.js

--- commands/up.js
+++ commands/up.js
@@ -3,10 +3,10 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-      let days = Math.floor(client.uptime / 86400000);
-      let hours = Math.floor(client.uptime / 3600000) % 24;
-      let minutes = Math.floor(client.uptime / 60000) % 60;
-      let seconds = Math.floor(client.uptime / 1000) % 60;
+      let days = Math.floor(bot.uptime / 86400000);
+      let hours = Math.floor(bot.uptime / 3600000) % 24;
+      let minutes = Math.floor(bot.uptime / 60000) % 60;
+      let seconds = Math.floor(bot.uptime / 1000) % 60;
 
       message.channel.send(`**Uptime:** ${days}d ${hours}h ${minutes}m ${seconds}s`);
 }
