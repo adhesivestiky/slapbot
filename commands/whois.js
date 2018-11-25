75612d9ddd81ff56d5cb01eef829744d3e9293f2
adhesivestiky <44936240+adhesivestiky@users.noreply.github.com>
2018-11-24 22:45:49 -0800

    Create whois.js

---
+++ commands/whois.js
@@ -0,0 +1,21 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+ let person = message.mentions.members.first() || message.guild.members.get(args[0]);
+ if(!person){
+  person = message.author
+ };
+ //define before embed
+ let userAvatar = person.displayAvatarURL;
+ let userCreated = person.createdAt;
+ let embed = new Discord.RichEmbed()
+ 
+ .setTitle("Info of ${person.tag}", userAvatar)
+ .setColor("#0fff00")
+ .addField("Created on" uCreated)
+ .addField("Joined at" person.joinedAt)
+}
+
+module.exports.help = {
+  name: "whois"
+}
