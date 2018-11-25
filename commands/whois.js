6f931d512df4b00679678f489c70a6a3d44947bc
adhesivestiky <44936240+adhesivestiky@users.noreply.github.com>
2018-11-25 09:46:35 -0800

    Update whois.js

--- commands/whois.js
+++ commands/whois.js
@@ -1,17 +1,27 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
- let user = message.mentions.members.first()
+ let user = message.mentions.members.first();
  if(!user){
   user = message.author
  };
+ 
+ let b = "boop";
+ 
+ if(user = message.author){
+  b = message.author.tag;
+ };
+ 
+if(!user = message.author){
+ b = "{I'm having trouble tagging other members}";
+};
 
  const member = message.guild.member(user);
  let userAvatar = user.displayAvatarURL;
  let userCreated = user.createdAt;
  let embed = new Discord.RichEmbed()
  
- .setAuthor(`Info of ${member.username}#${member.discriminator}`, userAvatar)
+ .setAuthor(`Info of ${b}`, member.displayAvatarURL)
  .setColor("#0fff00")
  .addField("Status", user.presence.status)
  .addField("Roles", member.roles.map(roles => roles.name).join(', '))
