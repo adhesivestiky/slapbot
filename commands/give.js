fc976423e2cbc5fe5905c45b7220d6ebb50f6e84
adhesivestiky <44936240+adhesivestiky@users.noreply.github.com>
2018-11-25 16:59:53 -0800

    Update give.js

--- commands/give.js
+++ commands/give.js
@@ -1,36 +1,44 @@
 const Discord = require("discord.js");
 const fs = require("fs");
-
+let money = require("../money.json");
 
 module.exports.run = async (bot, message, args) => {
- let money = require("../money.json");
- 
- if(!money[message.author.id]){
-  return message.reply("You don't even ***have*** any money... **NOTE**: Money resets when the bot gets updated and at this stage, it gets updated a lot. Some users are important so they get their money saved however :D");
- }
- 
- let pUser = message.mentions.members.first() || message.guild.members.get(args[0]);
- let pCoins = money[pUser.id]
- let sCoins = money[message.author.id]
- 
- if(sCoins < args[0]) return message.reply("You don't have enough money... feelsbadman");
- 
- money[message.author.id] = {
-  money: sCoins - parseInt(args[1])
- };
- 
- money[pUser.id] = {
-  money: pCoins + parseInt(args[1])
- };
- 
- message.channel.send("Money transfer complete!");
- 
- fs.writeFile("./money.json", JSON.stringify(money), (err) => {
-  if(err) console.log(err)
- });
- 
+  //!pay @isatisfied 59345
+
+  if(!money[message.author.id]){
+    return message.reply("You don't have any money!")
+  }
+
+  let pUser = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
+
+  if(!money[pUser.id]){
+    money[pUser.id] = {
+      money: 0
+    };
+  }
+
+  let pCoins = money[pUser.id].money;
+  let sCoins = money[message.author.id].money;
+
+  if(sCoins < args[0]) return message.reply("Not enough money!");
+
+  money[message.author.id] = {
+    money: sCoins - parseInt(args[1])
+  };
+
+  money[pUser.id] = {
+    money: pCoins + parseInt(args[1])
+  };
+
+  message.channel.send(`Transfer of money complete!`);
+
+  fs.writeFile("./money.json", JSON.stringify(money), (err) => {
+    if(err) console.log(err)
+  });
+
+
 }
 
 module.exports.help = {
- name: "give"
+  name: "give"
 }
