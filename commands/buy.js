ca25410c13780dc3b29c27f74ad84c1127b9308b
adhesivestiky <44936240+adhesivestiky@users.noreply.github.com>
2018-11-29 19:31:15 -0800

    Update buy.js

--- commands/buy.js
+++ commands/buy.js
@@ -12,7 +12,7 @@ module.exports.run = async (bot, message, args) => {
     let banana = message.guild.roles.find(`name`, "Banana");
     if(!banana) return message.reply("This server does not have a role named 'Banana'. Ask an admin to create one for you if you want this feature!");
 
-    if(rMember.roles.has(banana.id)){
+    if(message.author.roles.has(banana.id)){
       message.channel.send("But you already have a banana...");
     }
     
@@ -24,20 +24,23 @@ module.exports.run = async (bot, message, args) => {
   };
   
   if(item == "bombs" || "explosives"){
-    let banana = message.guild.roles.find(`name`, "Banana");
-    if(!banana) return message.reply("This server does not have a role named 'Explosives'. Ask an admin to create one for you if you want this feature!");
+    let bomb = message.guild.roles.find(`name`, "Banana");
+    if(!bomb) return message.reply("This server does not have a role named 'Explosives'. Ask an admin to create one for you if you want this feature!");
 
-    if(rMember.roles.has(banana.id)){
+    if(message.author.roles.has(bomb.id)){
       message.channel.send("But you already have explosives...");
     };
     
     await(message.author.addRole(banana.id));
     
     money[message.author.id] = {
-      money: message.author - 40
+      money: money[message.author].money - 40
     };
   };
     
+  fs.writeFile("./money.json", JSON.stringify(money), (err) => {
+    if(err) console.log(err)
+  });
 
   message.channel.send("Item purchased!");    
 }
