674401351a5682afafbbdfe5d6c52fa48ad7075a
adhesivestiky <44936240+adhesivestiky@users.noreply.github.com>
2018-12-01 14:02:38 -0800

    Update explode.js
    -Fixed bugs
    -Created statements saying how much damage is dealt

--- commands/explode.js
+++ commands/explode.js
@@ -3,20 +3,34 @@ const fs = require("fs");
 let hp = require("../health.json");
 
 module.exports.run = async (bot, message, args) => {
-  if(!message.member.roles.find(`name`, "Explosives")){
+  if(message.member.roles.find(`name`, "Explosives")){
     let user = message.mentions.members.first() || message.guild.members.get(args[0]); 
     let bomb = message.guild.roles.find(`name`, 'Explosives');
     let amount = Math.floor(Math.random() * 30);
+    let accuracy = '.';
+    //accuracy levels
+    if(amount == 0){
+      accuracy = `You miss and deal no damage.`
+    } else if(amount > 0 < 10){
+      accuracy = `You barely hit ${user}, dealing ${amount} damage to them. They now have ${hp[user.id].health - amount}/100 health left.`
+    } else if(amount > 11 < 20){
+      accuracy = `A decent hit. You dealt ${amount} damage with it!`
+    } else {
+      accuracy = `A direct hit, dealing ${amount} damage!`
+    };
   
       hp[user.id] = {
-        money: hp[user.id].health - amount
+        health: hp[user.id].health - amount
       };
       
       message.member.removeRole(bomb);
+      message.channel.send(`You throw some explosives. ${accuracy}`);
 
-    fs.writeFile("./money.json", JSON.stringify(money), (err) => {
+    fs.writeFile("./health.json", JSON.stringify(health), (err) => {
       if(err) console.log(err)
     });
+  } else {
+   message.channel.send("Buy explosives from the shop to use this!");
   };
 }
 
