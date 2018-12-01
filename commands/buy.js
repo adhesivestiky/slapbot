244c3a886406b725944d4ea9bbd57ac40aa11553
adhesivestiky <44936240+adhesivestiky@users.noreply.github.com>
2018-11-30 21:01:14 -0800

    Update buy.js

--- commands/buy.js
+++ commands/buy.js
@@ -12,7 +12,7 @@ module.exports.run = async (bot, message, args) => {
     let banana = message.guild.roles.find(`name`, "Banana");
     if(!banana) return message.reply("This server does not have a role named 'Banana'. Ask an admin to create one for you if you want this feature!");
     
-    await(message.author.addRole(banana.id));
+    await(message.member.addRole(`${banana.id}`));
     
     money[message.author.id] = {
       money: money[message.author].money - 30
@@ -23,7 +23,7 @@ module.exports.run = async (bot, message, args) => {
     let bomb = message.guild.roles.find(`name`, "Explosives");
     if(!bomb) return message.reply("This server does not have a role named 'Explosives'. Ask an admin to create one for you if you want this feature!");
     
-    await(message.author.addRole(bomb.id));
+    await(message.member.addRole(`${bomb.id}`));
     
     money[message.author.id] = {
       money: money[message.author].money - 40
