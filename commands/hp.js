1563294f48bdcfb0c17795e1237c033021ca8e33
adhesivestiky <44936240+adhesivestiky@users.noreply.github.com>
2018-11-30 21:23:29 -0800

    Create hp.js

---
+++ commands/hp.js
@@ -0,0 +1,32 @@
+const Discord = require("discord.js");
+let hp = require("../health.json");
+
+module.exports.run = async (bot, message, args) => {
+  
+  let member = message.mentions.members.first() || message.guild.members.get(args[0]);
+  if(!member){
+    member = message.author
+  };
+  
+ if(!hp[member.id]){
+    hp[member.id] = {
+      health: 0
+    };
+  };
+  
+  let uHp = hp[member.id].hp;
+  
+  let uAvatar = message.author.displayAvatarURL;
+  let hpEmbed = new Discord.RichEmbed()
+  .setColor("#00ff00")
+  .setDescription(`<@${member.id}> **has...** \n${uHp} money!`)
+  .setFooter(`Requested by ${message.author.tag}`, uAvatar)
+  .setTimestamp();
+  
+  message.channel.send(hpEmbed);
+  
+}
+
+module.exports.help = {
+  name: "hp"
+}
