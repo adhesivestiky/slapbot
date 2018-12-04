aa9425ea0d0849a78bb8f3cd54a52bfe1afde450
adhesivestiky <44936240+adhesivestiky@users.noreply.github.com>
2018-12-03 21:06:57 -0800

    Update userinfo.js

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -1,17 +1,22 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
- let user = message.mentions.members.first() || message.guild.members.get(args[0]); 
- if(!user){
-  user = message.author
- };
- 
- const member = message.guild.member(user);
+
+     let user = args.join(" ");
+	  if(!user){
+    user = message.author
+   };
+    const users = message.guild.users.array();
+    const matches = users.filter(user => user.name.toLowerCase().includes(args.join(" ").toLowerCase())) 
+          user = matches[0];
+	  if(matches.length === 1) {
+
+ let member = message.guild.member(user);
  let embed = new Discord.RichEmbed()
  
  .setAuthor(`User Info`, member.displayAvatarURL)
  .setDescription(`<@${member.id}>'s information`)
- .setColor("#0fff00")
+ .setColor(`${member.displayColor}`)
  .addField("Roles", member.roles.map(roles => roles.name).join(', '))
  .addField("Last message sent", member.lastMessage)
  .addField("Current Status", `Game: ${member.presence.game} (Null = No Game) \n Status: ${member.presence.status}`)
@@ -20,6 +25,17 @@ module.exports.run = async (bot, message, args) => {
 
  
  message.channel.send(embed);
+       
+       } else {
+    if(matches.length === 0) message.reply("couldn't find any users.");
+    if(matches.length >= 2) {
+       let failembed = new Discord.RichEmbed()
+       .setColor('##db0000')
+       .setAuthor('Failed:', 'https://cdn.discordapp.com/attachments/516507779738107919/519344315672166429/unknown.png')
+       .setDescription(`**Too many users:**/n${matches.join("\n")}`);       
+       
+       message.channel.send(failembed);
+	 };
 }
 
 module.exports.help = {
