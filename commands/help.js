03deade6407d5fa35ec3e11f8b61fe8cf9a91466
adhesivestiky <44936240+adhesivestiky@users.noreply.github.com>
2018-12-20 14:39:53 -0800

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,43 +1,34 @@
 const Discord = require("discord.js");
+const Info = reqiure("../package.json");
 
 module.exports.run = async (bot, message, args) => {
   let dev = "Developer commands are not shown to the public.";
   if(message.author.id == '306287412437450753'){
     dev = "These are commands you can use: \n\`.eval [code]\` \nThis runs a bit of code you type"
   };
-  
-  let n = "**NOTE:** Money resets when the bot gets updated, and at this beta stage, it gets updated a lot. Apologies for any inconviences this may cause, when the bot reaches a point, updates will be larger and less frequent.";
-  let embed = new Discord.RichEmbed()
-
-  .setTitle("Support Server")
-  .setAuthor("Help", "https://cdn.discordapp.com/avatars/501207321087836161/c75271b960d592b84f9fc54a522f19d8.png?size=2048")
-  .setColor("#00ffff")
-  .addField("**MAIN COMMANDS**", "Your regular old bot commands.")
-  .addField("\`.ping\`", "Tells how fast the bot responds to messages.")
-  .addField("\`.help\`", "Displays this menu.")
-  .addField("\`.info\`", "Displays some information about the bot")
-  .addField("\`.userinfo [user]\`", "Displays some basic information about the given user.")
-  .addField("\`.up\`", "Displays the bot's uptime")
-  .addField("**ENTERTAINMENT COMMANDS**", "All commands used for entertainment purposes. Have fun!")
-  .addField("\`.balance [user (optional)]\`", `Displays your cash balance (${n}).`)
-  .addField("\`.give [user] [amount]\`", `Give a user a certain amount of coins (${n}).`)
-  .addField("\`.buy [item]\`", "Buys a certain item.")
-  .addField("\`.shop\`", "Displays the list of all purchasable items!")
-  .addField("\`.explode [user]\`", "Takes away a bit of a user's health.")
-  .addField("**MODERATION COMMANDS**", "This includes any command that requires a certain permission to use.")
-  .addField("\`.lobby [text]\`", "Sends a message in the main channel of Tennessine (link in support server)")
-  .addField("**DEVELOPER COMMANDS**", `${dev}`)
-  .setFooter(`Requested by ${message.author.tag}`, message.author.displayAvatarURL)
-  .setURL("https://discord.gg/yANQDJJ")
+  let menuembed = new Discord.RichEmbed()
+  .setTitle('This is Blob II.')
+  .setDescription('React with \'🎋\' to see the entertainment menu')
+  .setColor('#4286f4')
+  .setFooter(`Version ${Info.version}.`)
   .setTimestamp();
   
-  if(message.channel.type === "dm"){
-   message.channel.send("Here you go!")
-  } else {
-     message.channel.send("I sent you a DM with all my commands. If you did not get it, enable DMs from server members and try again!")
+  const menu = await message.channel.send(menuembed);
+  menu.react('🎋');
+  if(message.reaction.emoji.name === '🎋' && message.reaction.count === '2'){
+    let funmenu = new Discord.RichEmbed()
+    .setTitle('**ENTERTAINMENT COMMANDS**')
+    .setColor('#4286f4')
+    .addField('\`.balance [user]\`', 'Checks the given user\'s balance. Gives yours if no user. **Money is earned by typing. The more active you are, the more you earn.**')
+    .addField('\`.give <user> <amount>\`', 'Gives the given user a given amount of money and subtracts it from your balance.')
+    .addField('\`.shop\`', 'Displays the items you can buy'.)
+    .addField('\`.buy <item>\`', 'Buys a specific item.')
+    .addField('\`.explode <user>\`', 'Takes some of a user\'s health away.')
+    .addField('\`.hp <user>\`', 'Checks a user\'s health, yours if none.')
+    .addField('\`.inv\`', 'Fact-telling command.');
+    menu.edit(funmenu);
   };
   
-  message.author.send(embed);
 }
 
 module.exports.help = {
